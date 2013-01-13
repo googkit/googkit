7968a000dd77937aebc2e6ca2a93c7684bbffbfb
cocopon <cocopon@me.com>
2013-01-13 13:03:35 +0900

    Improve behavior on Windows

--- tools/update_deps.py
+++ tools/update_deps.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     os.chdir(basedir)
 
-    os.system(DEPS_WRITER + ' --root_with_prefix="debug/js_dev ../../../../debug/js_dev" --output_file="debug/js_dev/deps.js"')
+    os.system('python %s --root_with_prefix="debug/js_dev ../../../../debug/js_dev" --output_file="debug/js_dev/deps.js"' % DEPS_WRITER)
