8d09698adc4d3d5b0af4c033bd71b4d78bccb94d
cocopon <cocopon@me.com>
2013-01-13 13:03:15 +0900

    Fix indent

--- tools/sub/compile_index.py
+++ tools/sub/compile_index.py
@@ -9,13 +9,13 @@ def compile_index_html(path, compiled_js_path):
     lines = []
 
     for line in open(path):
-	    # Remove lines that requires unneeded scripts
+        # Remove lines that requires unneeded scripts
         if re.search(r'closure/goog/base\.js', line) is not None:
             continue
         if re.search(r'goog\.require', line) is not None:
             continue
 
-	    # Replace deps.js by a compiled script
+        # Replace deps.js by a compiled script
         line = re.sub(r'js_dev/deps\.js', compiled_js_path, line)
         lines.append(line)
 
