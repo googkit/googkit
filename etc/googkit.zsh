bc9bd8e2abca4c5a5c463428340777961e033cfe
cocopon <cocopon@me.com>
2013-10-05 15:50:14 +0900

    Improve keyword detecting mechanism of zsh completion

--- etc/googkit.zsh
+++ etc/googkit.zsh
@@ -4,13 +4,9 @@ _googkit() {
 
 	total_words=${#words}
 	if [[ $total_words -eq 2 ]]; then
-		completions=$'compile\nconfig\ndeps\ninit\nsetup\nupdate'
-	elif [[ $total_words -eq 3 ]]; then
-		if [[ $words[2] = 'config' ]]; then
-			completions='update'
-		elif [[ $words[2] = 'deps' ]]; then
-			completions='update'
-		fi
+		completions="$(googkit commands)"
+	else
+		completions="$(googkit commands ${words[2,-2]})"
 	fi
 
 	reply=("${(ps:\n:)completions}")
