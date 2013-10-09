54547c24fe0ca2f04fb4077e801f47f37e884b87
OrgaChem <orga.chem.job@gmail.com>
2013-10-09 19:11:32 +0900

    Add a test for command.base_command

---
+++ test/command/test_base_command.py
@@ -0,0 +1,83 @@
+# Run the following command to test:
+#
+#     (in /usr/local/googkit)
+#     $ python -m {test_module_name}
+#
+# See also: http://docs.python.org/3.3/library/unittest.html#command-line-interface
+#
+# We cannot use unittest.mock on python 2.x!
+# Please install the Mock module when you use Python 2.x.
+#
+#     $ easy_install -U Mock
+#
+# See also: http://www.voidspace.org.uk/python/mock/#installing
+
+import unittest
+
+try:
+    # Python 3.3 or later
+    import unittest.mock as mock
+except ImportError:
+    # Python 2.x or 3.2-
+    import mock
+
+from lib.error import GoogkitError
+from command.base_command import BaseCommand
+
+
+class EnvironmentStub(object):
+    def __init__(self):
+        self.config = None
+
+
+class TestBaseCommand(unittest.TestCase):
+    # run {{{
+    def test_run_on_cmd(self):
+        class ConcreteCommand(BaseCommand):
+            pass
+
+        env1 = EnvironmentStub()
+        env1.config = mock.MagicMock()
+
+        cmd1 = ConcreteCommand(env1)
+        cmd1.run_internal = mock.MagicMock()
+        cmd1.run()
+        cmd1.run_internal.assert_called_once_with()
+
+        env2 = EnvironmentStub()
+        env2.config = None
+
+        cmd2 = ConcreteCommand(env2)
+        cmd2.run_internal = mock.MagicMock()
+        cmd2.run()
+        cmd2.run_internal.assert_called_once_with()
+
+
+    def test_run_on_cmd_needs_config(self):
+        class ConcreteCommandNeedsConfig(BaseCommand):
+            @classmethod
+            def needs_config(cls):
+                return True
+
+        env1 = EnvironmentStub()
+        env1.config = mock.MagicMock()
+
+        cmd1 = ConcreteCommandNeedsConfig(env1)
+        cmd1.run_internal = mock.MagicMock()
+        cmd1.run()
+        cmd1.run_internal.assert_called_once_with()
+
+        env2 = EnvironmentStub()
+        env2.config = None
+
+        cmd2 = ConcreteCommandNeedsConfig(env2)
+        cmd2.run_internal = mock.MagicMock()
+        with self.assertRaises(GoogkitError):
+            cmd2.run()
+    # }}}
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vim: fdm=marker
