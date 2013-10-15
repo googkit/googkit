c2842c6285d6af92f94ebc8327e3317fc17bde9f
cocopon <cocopon@me.com>
2013-10-15 18:43:06 +0900

    Refactor test_clone

--- test/lib/test_clone.py
+++ test/lib/test_clone.py
@@ -7,36 +7,46 @@ except ImportError:
     # Python 2.x or 3.2-
     import mock
 
-import subprocess
 import googkit.lib.clone
 
 
 class TestClone(unittest.TestCase):
     def test_run(self):
+        with mock.patch('os.path.exists', return_value=True), \
+                mock.patch('googkit.lib.clone._pull') as mock_pull:
+            googkit.lib.clone.run('https://exmaple.com/example.git', '/dir1/dir2')
+
+        mock_pull.assert_called_once()
+
+    def test_run_with_no_target_dir(self):
+        with mock.patch('os.path.exists', return_value=False), \
+                mock.patch('googkit.lib.clone._clone') as mock_clone:
+            googkit.lib.clone.run('https://exmaple.com/example.git', '/dir1/dir2')
+
+        mock_clone.assert_called_once()
+
+    def test_pull(self):
         MockPopen = mock.MagicMock()
         MockPopen.return_value.returncode = 0
 
         with mock.patch('os.getcwd', return_value='/dir1/dir2/dir3/dir4'), \
-                mock.patch('os.path.exists', return_value=True), \
                 mock.patch('os.chdir') as mock_chdir, \
                 mock.patch('subprocess.Popen', new=MockPopen) as mock_popen:
-            googkit.lib.clone.run('https://exmaple.com/example.git', '/dir1/dir2')
+            googkit.lib.clone._pull('https://exmaple.com/example.git', '/dir1/dir2')
 
         mock_chdir.assert_any_call('/dir1/dir2')
         mock_chdir.assert_any_call('/dir1/dir2/dir3/dir4')
-        mock_popen.assert_called_once_with(['git', 'pull'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        mock_popen.assert_called_once()
 
-    def test_run_with_no_target_dir(self):
+    def test_clone(self):
         MockPopen = mock.MagicMock()
         MockPopen.return_value.returncode = 0
 
-        with mock.patch('os.getcwd', return_value='/dir1/dir2/dir3/dir4'), \
-                mock.patch('os.path.exists', return_value=False), \
-                mock.patch('os.chdir'), \
+        with mock.patch('os.chdir'), \
                 mock.patch('subprocess.Popen', new=MockPopen) as mock_popen:
             googkit.lib.clone.run('https://example.com/example.git', '/dir1/dir2')
 
-        mock_popen.assert_called_once_with(['git', 'clone', 'https://example.com/example.git', '/dir1/dir2'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        mock_popen.assert_called_once()
 
 
 if __name__ == '__main__':
