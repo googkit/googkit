852f0b51f232019c28fecb8ff301585ad8046392
cocopon <cocopon@me.com>
2013-10-15 18:41:34 +0900

    Refactor googkit.lib.clone

--- googkit/lib/clone.py
+++ googkit/lib/clone.py
@@ -6,9 +6,17 @@ from googkit.lib.error import GoogkitError
 
 def run(repos, target_path):
     if os.path.exists(target_path):
+        _pull(repos, target_path)
+    else:
+        _clone(repos, target_path)
+
+
+def _pull(repos, target_path):
+    cwd = os.getcwd()
+    os.chdir(target_path)
+
+    try:
         args = ['git', 'pull']
-        cwd = os.getcwd()
-        os.chdir(target_path)
 
         proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         result = proc.communicate()
@@ -17,14 +25,16 @@ def run(repos, target_path):
             raise GoogkitError('Git pull failed: ' + result[1])
 
         logging.debug(result[0])
-
+    finally:
         os.chdir(cwd)
-    else:
-        args = ['git', 'clone', repos, target_path]
-        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        result = proc.communicate()
 
-        if proc.returncode != 0:
-            raise GoogkitError('Git clone failed: ' + result[1])
 
-        logging.debug(result[0])
+def _clone(repos, target_path):
+    args = ['git', 'clone', repos, target_path]
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = proc.communicate()
+
+    if proc.returncode != 0:
+        raise GoogkitError('Git clone failed: ' + result[1])
+
+    logging.debug(result[0])
