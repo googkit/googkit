fdb68d2a70cc0ae167af3e27b340be6fae0f4a25
cocopon <cocopon@me.com>
2013-10-20 12:36:22 +0900

    Improve web app template, close #29

--- etc/template/development/js_dev/example.js
+++ etc/template/development/js_dev/example.js
@@ -12,23 +12,31 @@ foo.Example = function() {
 };
 
 
-/**
- * Does something.
- */
-foo.Example.prototype.doSomething = function() {
-  var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  var expr = nums.join(' + ');
-  var total = 0;
-  var elem;
+foo.Example.prototype.showItWorks = function() {
+  // Add 'works' css class if Closure Library works correctly
+  goog.dom.classes.set(document.body, 'works');
+};
 
+
+foo.Example.prototype.demonstrate = function() {
+  var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var expression = nums.join(' + ');
+  var total = 0;
   goog.array.forEach(nums, function(num) {
     total += num;
   }, this);
 
-  elem = goog.dom.createDom(
-      'p',
-      null,
-      expr + ' = ' + String(total));
+  var welcomeElem = goog.dom.getElementByClass('welcome');
+  var text = expression + ' = ' + String(total);
+  var newElem = goog.dom.createDom('pre', null, text);
+  goog.dom.insertSiblingAfter(newElem, welcomeElem);
+};
 
-  goog.dom.append(document.body, elem);
+
+/**
+ * Does something.
+ */
+foo.Example.prototype.doSomething = function() {
+  this.showItWorks();
+  this.demonstrate();
 };
