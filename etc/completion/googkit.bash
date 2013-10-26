0bd541251ca8c2edeb0c94cce0b973e502d06a9c
cocopon <cocopon@me.com>
2013-10-26 16:44:54 +0900

    Add CandidatesCommand instead of CommandsCommand

--- etc/completion/googkit.bash
+++ etc/completion/googkit.bash
@@ -3,12 +3,12 @@ _googkit() {
 	local word="${COMP_WORDS[COMP_CWORD]}"
 
 	if [ "$COMP_CWORD" -eq 1 ]; then
-		COMPREPLY=( $(compgen -W "$(googkit _commands)" -- "$word") )
+		COMPREPLY=( $(compgen -W "$(googkit _candidates)" -- "$word") )
 	else
 		local words=("${COMP_WORDS[@]}")
 		unset words[0]
 		unset words[$COMP_CWORD]
-		local completions=$(googkit _commands "${words[@]}")
+		local completions=$(googkit _candidates "${words[@]}")
 		COMPREPLY=( $(compgen -W "$completions" -- "$word") )
 	fi
 }
