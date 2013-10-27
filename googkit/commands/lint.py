66da77d991e179ce6eacfe524e40d88ab2fcd210
OrgaChem <orga.chem.job@gmail.com>
2013-10-28 04:19:22 +0900

    Implement a hint message

--- googkit/commands/lint.py
+++ googkit/commands/lint.py
@@ -13,6 +13,20 @@ class LintCommand(Command):
     def needs_project_config(cls):
         return True
 
+    @classmethod
+    def is_linter_installed(cls):
+        popen_args = {
+            'shell': True,
+            'stdout': subprocess.PIPE,
+            'stderr': subprocess.PIPE,
+        }
+
+        try:
+            subprocess.check_call(['gjslint', '--version'], **popen_args)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
     def _sources(self):
         js_dev = self.config.js_dev_dir()
         deps_js = self.config.deps_js()
@@ -36,7 +50,7 @@ class LintCommand(Command):
 
         popen_args = {
             'stdout': subprocess.PIPE,
-            'stderr': subprocess.PIPE
+            'stderr': subprocess.PIPE,
         }
 
         proc = subprocess.Popen(cmd, **popen_args)
@@ -45,6 +59,13 @@ class LintCommand(Command):
         logging.info(result[0].decode())
 
     def run_internal(self):
+        if not LintCommand.is_linter_installed():
+            logging.error('[Error] Closure Linter is not found.\n' +
+                          '\n' +
+                          'Did you install Closure Linter?\n' +
+                          'Try: sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz')
+            return
+
         project_root = googkit.lib.path.project_root(self.env.cwd)
         with working_directory(project_root):
             self.lint()
